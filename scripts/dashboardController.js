0eeffb47eaf95a10a54c5b05a22e5879c93e1c8f
Jesse Rountree <salty_sea_rat@hotmail.com>
2017-12-18 20:51:24 -0800

    updated again

--- scripts/dashboardController.js
+++ scripts/dashboardController.js
@@ -1,11 +1,10 @@
 var ngDashboard = angular
 	.module('ngDashboard',[]);
 
-var $monthname = "";
-
+   //Animations
 	ngDashboard.controller('dashboardController', ['$scope', '$log', function($scope, $log){
 		$scope.slider = function(){ //SLIDE PAGE OUT
-	   		$scope.class = "slideout";
+	   		$scope.homeSlider = "slideout";
 	   		$scope.returnclass = "btnup";
 	   		$scope.wkanimate = "wkanimatein";
 	   		$scope.baranimate = "baranimatein";
@@ -29,116 +28,31 @@ var $monthname = "";
 			$scope.monthdrop = "menudropup";
 	}
 
-		$scope.weekSelectClick = function(){
-			$log.log("Week List");
-	}
-		$scope.monthSelectClick = function(){
-			$log.log($monthname);
-	}
-	//Week selection in graph
-		$scope.week1SelectClick = function(){
-			$log.log("Week 1 Select");
-	}
-		$scope.week2SelectClick = function(){
-			$log.log("Week 2 Select");
-	}
-		$scope.week3SelectClick = function(){
-			$log.log("Week 3 Select");
-	}
-		$scope.week4SelectClick = function(){
-		$log.log("Week 4 Select");
-	}
-		$scope.week5SelectClick = function(){
-		$log.log("Week 5 Select");
-	}
-		$scope.week6SelectClick = function(){
-		$log.log("Week 6 Select");
-	}
+}]) // END Animations
 
-	//Month selection in graph
-		$scope.month1SelectClick = function(){
-		var monthname = document	
-		$log.log("January");
-	}
-		$scope.month2SelectClick = function(){
-		$log.log("February");
-	}
-		$scope.month3SelectClick = function(){
-		$log.log("March");
-	}
-		$scope.month4SelectClick = function(){
-		$log.log("April");
-	}
-		$scope.month5SelectClick = function(){
-		$log.log("May");
-	}
-		$scope.month6SelectClick = function(){
-		$log.log("June");
-	}	
-		$scope.month7SelectClick = function(){
-		$log.log("July");
-	}
-		$scope.month8SelectClick = function(){
-		$log.log("August");
-	}
-		$scope.month9SelectClick = function(){
-		$log.log("September");
-	}
-		$scope.month10SelectClick = function(){
-		$log.log("October");
-	}
-		$scope.month11SelectClick = function(){
-		$log.log("November");
-	}
-		$scope.month12SelectClick = function(){
-		$log.log("December");
-	}
-}]) // END dashboardController
-
-
-
-/* live version
+	//Month Selections and Data Pulls
 	ngDashboard.controller('monthController', function($scope, dashboardFactory) {
-
+		$scope.monthdisplay = currentMonth;
+		//Month data pull
 		$scope.monthdata;
-
 		dashboardFactory.getMonthData().then(function(data) {
 			$scope.monthdata = data.data;
+			
 			console.log('This is a Success.  GOOD FOR YOU BITCH');
-
+			console.log(currentMonth);
 		}, function(error) {
 			console.log(error);
 			console.log("This is an Error because you suck and should become a garbage man you garbage human.");
 		});﻿
+		//Month Selection
+		$scope.selected_value;
+		$scope.monthSelection;
+		$scope.update = function() {
+			console.log("working");
+			$scope.monthSelection = $scope.selected_value;
+			$scope.monthdisplay = $scope.monthSelection;
+			console.log($scope.selected_value + " selection");
+		}
 
+}) // END SCRIPT
 
-})
-*/
-
-/*angular
-	.module('ngDashboard')
-	.controller('dashboardController', function($scope, dashboardFactory) {
-		
-		$scope.weekSelect;
-
-		dashFactory.getDashboard().then(function(data) {
-			$scope.weekSelect = data.data;
-				}, function(error) {
-					console.log(error)
-				});﻿
-	});
-
-*/
-/*
-	ngDashboard.controller('monthDataController', function($scope, dashboardFactory) {
-		
-		$scope.monthData;
-
-		dashboardFactory.getMonthData().then(function(data) {
-			$scope.MonthData = data.data;
-		}, function(error) {
-			console.log(error)
-		});﻿
-
-	});
-*/
